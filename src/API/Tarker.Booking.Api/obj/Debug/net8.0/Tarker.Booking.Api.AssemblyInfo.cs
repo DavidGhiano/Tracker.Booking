118e92aadd12ba056f659bfe12b8f6df6a8b8ee3
David Ghiano <davidghiano@gmail.com>
2025-05-04 00:03:00 -0300

    Commit para git

--- src/API/Tarker.Booking.Api/obj/Debug/net8.0/Tarker.Booking.Api.AssemblyInfo.cs
+++ src/API/Tarker.Booking.Api/obj/Debug/net8.0/Tarker.Booking.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tarker.Booking.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eed74628598bbfd8068a825ff27131824fa08e1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tarker.Booking.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tarker.Booking.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Common/Tarker.Booking.Common/obj/Debug/net8.0/Tarker.Booking.Common.AssemblyInfo.cs
+++ src/Common/Tarker.Booking.Common/obj/Debug/net8.0/Tarker.Booking.Common.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tarker.Booking.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eed74628598bbfd8068a825ff27131824fa08e1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tarker.Booking.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tarker.Booking.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
